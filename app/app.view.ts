85f1e4af30dfec009dcc93e8e6cdaf7eac459f32
Lyumih <m.ivanov1225@gmail.com>
2024-01-14 04:44:58 +0300

    add hero

--- app/app.view.ts
+++ app/app.view.ts
@@ -4,20 +4,10 @@ namespace $.$$ {
 		@$mol_mem
 		party(): readonly $gen_engine_unit[] {
 			return [
-				this.$.$gen_engine_unit.make( { name: () => 'Вася' } ),
-				this.$.$gen_engine_unit.make( { name: () => 'Даша', level: () => 10 } ),
-				this.$.$gen_engine_unit.make( {} ),
-			]
-		}
-
-		@$mol_mem
-		common_party() {
-			return [
 				this.$.$gen_engine_unit.make( { name: () => 'Вася', type: () => 'hero' } ),
-				this.$.$gen_engine_unit.make( { name: () => 'Даша', level: () => 10 } ),
-				this.$.$gen_engine_unit.make( {} ),
+				this.$.$gen_engine_unit.make( { name: () => 'Даша', type: () => 'hero', level: () => 10 } ),
+				this.$.$gen_engine_unit.make( { type: () => 'hero' } ),
 			]
 		}
-
 	}
 }
